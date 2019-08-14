13d63c7e31c6bf40328d6ed7fb8e568a1169a930
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-08-14 11:00:55 -0400

    set window state object

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const url = require('url')
 const path = require('path')
 
@@ -18,20 +20,26 @@ beforeEach(() => {
   })
   console.log('index file url "%s"', indexFileUrl)
 
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const replace = true
     // let the test window open the electron main window
-    const mw = open('http://localhost:4600', 'mainWindow')
+    const mw = (window.mw = open(
+      'http://localhost:4600',
+      'mainWindow',
+      replace
+    ))
     // const mw = window.mw = open(indexFileUrl, 'mainWindow', replace)
 
     // if the proxy injects "document.domain = localhost"
     // then remote websites should work
-    // const mw = window.mw = open('http://todomvc.com/examples/vue/', 'mainwindow')
+    // const mw = window.mw = open('http://todomvc.com/examples/vue/', 'mainwindow', replace)
+    // const mw = window.mw = open('https://example.cypress.io/', 'mainwindow', replace)
 
     // TODO resolve when mw.document is valid
     // for now simply wait
     setTimeout(() => {
       cy.state('document', mw.document)
+      cy.state('window', mw)
       resolve()
     }, 100)
   })
@@ -45,12 +53,18 @@ it('works', () => {
   // cy.wrap('electron.js').should('equal', 'electron.js')
   cy.get('#node-version').should('have.text', '12.4.0')
   cy.contains('Static page')
-  cy.get('button').click().click()
 
-  cy.get('input').type('Test typing').should('have.value', 'Test typing')
+  cy.window().its('console').then(console => cy.spy(console, 'log').as('console-log'))
+  cy.get('button')
+    .click()
+    .click()
+
+  cy.get('input')
+    .type('Test typing')
+    .should('have.value', 'Test typing')
 })
 
 // for TodoMVC
-it.skip('adds todos', () => {
-  cy.get('.new-todo').type('one{enter}two{enter}')
-})
+// it.skip('adds todos', () => {
+//   cy.get('.new-todo').type('one{enter}two{enter}')
+// })
